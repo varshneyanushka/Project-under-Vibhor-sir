0b70419e0de4eb9bfbecb7500b9bfe9585f040f4
varshneyanushka <anushka.pkg@gmail.com>
2025-01-14 02:45:58 +0530

    tardiness

--- barchart.js
+++ barchart.js
@@ -1,5 +1,5 @@
 let videoTime, totalDuration, table;
-
+let designcycletime=0;
 function startTimer() {
   if (isRunning) return;
   isRunning = true;
@@ -24,7 +24,7 @@ videoElement.addEventListener("pause", function () {
   pauseTimer();
 });
 
-let rowi = 10;
+let rowi = 9;
 
 let ctx;
 let barLineChart;
@@ -100,19 +100,25 @@ async function loadExcelAndGenerateCharts() {
       .map((row) => row[lastHeaderIndex])
       .filter(Number.isFinite);
 
-    const { gaussianData: lastGaussianData10, limit1: lastlimit1, limit2: lastlimit2} =
-      calculateGaussian(lastColumnData.slice(0, rowi));
+    const {
+      gaussianData: lastGaussianData10,
+      limit1: lastlimit1,
+      limit2: lastlimit2,
+    } = calculateGaussian(lastColumnData.slice(0, rowi));
     const lastPeak10 = getPeakXValue(lastGaussianData10);
+    designcycletime=lastColumnData[rowi+2];
 
     const lastX11 = lastColumnData[rowi + 1];
-    const lastIsDanger = lastX11 < lastlimit2 && lastX11 >lastlimit1;
+    const lastIsDanger = lastX11 < lastlimit2 && lastX11 > lastlimit1;
 
     createChart(
       lastHeader,
       lastGaussianData10,
       null, // Assuming no 11-point Gaussian data
       lastX11,
-      lastIsDanger,lastlimit1,lastlimit2
+      lastIsDanger,
+      lastlimit1,
+      lastlimit2
     );
 
     // Process all headers except the last one
@@ -120,15 +126,28 @@ async function loadExcelAndGenerateCharts() {
       const columnData = columns
         .map((row) => row[index])
         .filter(Number.isFinite);
+        
 
-      const { gaussianData: gaussianData10, limit1: lastlimit1, limit2: lastlimit2 } =
-        calculateGaussian(columnData.slice(0, rowi));
+      const {
+        gaussianData: gaussianData10,
+        limit1: lastlimit1,
+        limit2: lastlimit2,
+      } = calculateGaussian(columnData.slice(0, rowi));
       const peak10 = getPeakXValue(gaussianData10);
+      
 
       const x11 = columnData[rowi + 1];
-      const isDanger = x11 < lastlimit2 && x11>lastlimit1;
+      const isDanger = x11 < lastlimit2 && x11 > lastlimit1;
 
-      createChart(header, gaussianData10, null, x11, isDanger,lastlimit1,lastlimit2);
+      createChart(
+        header,
+        gaussianData10,
+        null,
+        x11,
+        isDanger,
+        lastlimit1,
+        lastlimit2
+      );
     });
   } catch (error) {
     console.error("Error loading or processing the Excel file:", error);
@@ -148,11 +167,11 @@ function calculateGaussian(data) {
       Math.exp(-0.5 * Math.pow((x - mean) / stdDev, 2));
     gaussianData.push({ x, y });
   }
-  const limit1=mean-stdDev;
-  const limit2=mean+stdDev;
+  const limit1 = mean - stdDev;
+  const limit2 = mean + stdDev;
 
   // Return both Gaussian data and standard deviation
-  return { gaussianData, limit1,limit2 };
+  return { gaussianData, limit1, limit2 };
 }
 
 function getPeakXValue(gaussianData) {
@@ -164,104 +183,123 @@ function getPeakXValue(gaussianData) {
 
 let hr = 1;
 
-function createChart(header, gaussianData10, gaussianData11, x11, isDanger,lastlimit1,lastlimit2) {
-    const chartContainer = document.createElement("div");
-    chartContainer.classList.add("chart-grid");
-    const canvas = document.createElement("canvas");
-    chartContainer.appendChild(canvas);
-    document.getElementById("chartsContainer").appendChild(chartContainer);
-  
-    if (isDanger) {
-      delays.push(header);
-    }
-  
-    const colors = {
-      backgroundColor: isDanger
-        ?  "rgba(0, 192, 36, 1)":"rgba(255, 0, 0, 1)",
-      borderColor: isDanger ? "rgba(0, 192, 36, 0.63)": "rgba(255, 0, 0, 1)",
-    };
-  
-    new Chart(canvas.getContext("2d"), {
-        type: "line",
-        data: {
-          datasets: [
-            {
-              label: `Overall`,
-              data: gaussianData10,
-              backgroundColor: "rgba(0, 4, 255, 0.1)",
-              borderColor: "rgba(0, 4, 255, 0.59)",
-              fill: true,
-            },
-            {
-              label:'cycle time',
-              data: [{ x: x11, y: 0 }, { x: x11, y: Math.max(...gaussianData10.map(d => d.y)) }],
-              borderColor: colors.backgroundColor, // Vertical line color
-              borderWidth: 2,
-              
-              fill: false,
-              showLine: true,
-            },
+function createChart(
+  header,
+  gaussianData10,
+  gaussianData11,
+  x11,
+  isDanger,
+  lastlimit1,
+  lastlimit2
+) {
+  const chartContainer = document.createElement("div");
+  chartContainer.classList.add("chart-grid");
+  const canvas = document.createElement("canvas");
+  chartContainer.appendChild(canvas);
+  document.getElementById("chartsContainer").appendChild(chartContainer);
 
-            {
-              label:'limit',
-              data: [{ x: lastlimit1, y: 0 }, { x: lastlimit1, y: Math.max(...gaussianData10.map(d => d.y)) }],
-              borderColor:"rgba(99, 99, 99, 0.5)", // Vertical line color
-              borderWidth: 2,
-              
-              fill: false,
-              showLine: true,
-            },
-            {
-              label:'limit',
-              data: [{ x: lastlimit2, y: 0 }, { x: lastlimit2, y: Math.max(...gaussianData10.map(d => d.y)) }],
-              borderColor: "rgba(99, 99, 99, 0.5)", // Vertical line color
-              borderWidth: 2,
-              
-              fill: false,
-              showLine: true,
-            },
-            
+  if (isDanger) {
+    delays.push(header);
+  }
+
+  const colors = {
+    backgroundColor: isDanger ? "rgba(0, 192, 36, 1)" : "rgba(255, 0, 0, 1)",
+    borderColor: isDanger ? "rgba(0, 192, 36, 0.63)" : "rgba(255, 0, 0, 1)",
+  };
+
+  new Chart(canvas.getContext("2d"), {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: `Overall`,
+          data: gaussianData10,
+          backgroundColor: "rgba(0, 4, 255, 0.1)",
+          borderColor: "rgba(0, 4, 255, 0.59)",
+          fill: true,
+        },
+        {
+          label: "cycle time",
+          data: [
+            { x: x11, y: 0 },
+            { x: x11, y: Math.max(...gaussianData10.map((d) => d.y)) },
           ],
+          borderColor: colors.backgroundColor, // Vertical line color
+          borderWidth: 2,
+          fill: false,
+          showLine: true,
         },
-        options: {
-          responsive: true,
-          plugins: {
-            // legend: {
-            //   display: false, // Hides the legend completely
-            // },
-            tooltip: {
-                callbacks: {
-                  label: function (tooltipItem) {
-                    if (tooltipItem.dataset.label === "cycle time") {
-                      return `x: ${tooltipItem.raw.x.toFixed(4)}, y: ${tooltipItem.raw.y.toFixed(2)}`; // Show x and y for cycle time
-                    }
-                    return `x: ${tooltipItem.raw.x.toFixed(4)}, y: ${tooltipItem.raw.y.toFixed(2)}`; // General case
-                  },
-                },
-              },
-              
-          },
-          scales: {
-            x: {
-              title: {
-                display: true,
-                text: header,
-                color: "black",
-                font: {
-                  weight: "bold",
-                  size: 13,
-                },
-              },
-              type: "linear",
-            },
-            y: {
-              beginAtZero: true,
+        {
+          label: "limit", // First limit line
+          data: [
+            { x: lastlimit1, y: 0 },
+            { x: lastlimit1, y: Math.max(...gaussianData10.map((d) => d.y)) },
+          ],
+          borderColor: "rgba(99, 99, 99, 0.5)", // Vertical line color
+          borderWidth: 2,
+          fill: false,
+          showLine: true,
+        },
+        {
+          label: "limit", // Second limit line
+          data: [
+            { x: lastlimit2, y: 0 },
+            { x: lastlimit2, y: Math.max(...gaussianData10.map((d) => d.y)) },
+          ],
+          borderColor: "rgba(99, 99, 99, 0.5)", // Vertical line color
+          borderWidth: 2,
+          fill: false,
+          showLine: true,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          labels: {
+            // Filter legend items to hide the "limit" labels
+            filter: function (legendItem, chartData) {
+              return legendItem.text !== "limit"; // Hide "limit" labels from the legend
             },
           },
         },
-      });
-      
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              // Hide tooltips for "limit" datasets
+              if (tooltipItem.dataset.label === "limit") {
+                return null; // No tooltip for "limit"
+              }
+              // Show tooltip for other datasets
+              return `x: ${tooltipItem.raw.x.toFixed(
+                4
+              )}, y: ${tooltipItem.raw.y.toFixed(2)}`;
+            },
+          },
+        },
+      },
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: header,
+            color: "black",
+            font: {
+              weight: "bold",
+              size: 13,
+            },
+          },
+          type: "linear",
+        },
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
   
+
   if (hr === 1) {
     const horizontalLine = document.createElement("div");
     horizontalLine.className = "horizontal-line";
@@ -269,6 +307,12 @@ function createChart(header, gaussianData10, gaussianData11, x11, isDanger,lastl
   }
   hr++;
 }
+
+let noofjobsrem = 10;
+const expecteddeliverytime = 540;
+let timeremaining = 540;
+let tardiness = 0;
+
 
 function populateRandomDetails() {
   const today = new Date();
@@ -293,10 +337,10 @@ function populateRandomDetails() {
 
   // Set values
   document.getElementById("date-column").textContent = formattedDate;
-  document.getElementById("time-column").textContent = formattedTime;
-  document.getElementById("shift-column").textContent = `3 Shift`;
-  document.getElementById("remaining-shift-time-column").textContent = `2 hrs`;
-  document.getElementById("workpiece-column").textContent = `Workpiece s`;
+  document.getElementById("jobs").textContent = noofjobsrem-1;
+  document.getElementById("shift-column").textContent =expecteddeliverytime + " seconds";
+  document.getElementById("remaining-shift-time-column").textContent =timeremaining + " seconds";
+  document.getElementById("tardiness").textContent = tardiness + " seconds";
 }
 
 let headers = [];
@@ -305,7 +349,6 @@ let curr = 0;
 let timerInterval = null;
 let isRunning = false;
 let hi = 0;
-
 let starttime = 0;
 let serial = 0;
 
@@ -325,69 +368,134 @@ document.addEventListener("keydown", function (event) {
 });
 
 function handleNoMoreWorkpieces() {
+  // Clear the current content
   document.body.innerHTML = "";
 
+  // Create a container for the loading screen
   const loadingContainer = document.createElement("div");
   loadingContainer.style.display = "flex";
   loadingContainer.style.flexDirection = "column";
   loadingContainer.style.justifyContent = "center";
   loadingContainer.style.alignItems = "center";
   loadingContainer.style.height = "100vh";
-  loadingContainer.style.backgroundColor = "#f4f4f4";
+  loadingContainer.style.backgroundColor = "#eef2f3";
   loadingContainer.style.textAlign = "center";
+  loadingContainer.style.fontFamily = "'Poppins', sans-serif";
 
+  // Create a spinner with a gradient for better aesthetics
   const spinner = document.createElement("div");
-  spinner.style.border = "6px solid #ddd";
-  spinner.style.borderTop = "6px solid #007BFF";
+  spinner.style.border = "6px solid #e0e0e0";
+  spinner.style.borderTop = "6px solid #3498db";
   spinner.style.borderRadius = "50%";
-  spinner.style.width = "50px";
-  spinner.style.height = "50px";
+  spinner.style.width = "60px";
+  spinner.style.height = "60px";
   spinner.style.animation = "spin 1s linear infinite";
   loadingContainer.appendChild(spinner);
 
+  // Add a loading text
   const loadingText = document.createElement("p");
   loadingText.textContent = "Processing...";
-  loadingText.style.marginTop = "15px";
-  loadingText.style.fontSize = "18px";
-  loadingText.style.fontWeight = "bold";
-  loadingText.style.color = "#333";
+  loadingText.style.marginTop = "20px";
+  loadingText.style.fontSize = "20px";
+  loadingText.style.fontWeight = "500";
+  loadingText.style.color = "#555";
   loadingContainer.appendChild(loadingText);
 
+  // Append the loading container to the body
   document.body.appendChild(loadingContainer);
 
+  // Transition to the results screen after a delay
   setTimeout(() => {
+    // Clear the loading screen
     document.body.innerHTML = "";
 
+    // Create a container for the message screen
     const messageContainer = document.createElement("div");
     messageContainer.style.display = "flex";
     messageContainer.style.flexDirection = "column";
     messageContainer.style.justifyContent = "center";
     messageContainer.style.alignItems = "center";
     messageContainer.style.height = "100vh";
-    messageContainer.style.backgroundColor = "#f4f4f4";
+    messageContainer.style.backgroundColor = "#fdfdfd";
     messageContainer.style.textAlign = "center";
+    messageContainer.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+    messageContainer.style.borderRadius = "12px";
+    messageContainer.style.padding = "20px";
+    messageContainer.style.fontFamily = "'Poppins', sans-serif";
 
-    const message = document.createElement("h1");
-    message.textContent =
-      "No more workpieces left to be processed. Reload the page to review analysis";
-    message.style.color = "#333";
-    message.style.fontSize = "24px";
-    message.style.fontWeight = "bold";
-    message.style.marginBottom = "20px";
-    messageContainer.appendChild(message);
+    // First message
+    const message1 = document.createElement("p");
+    message1.textContent = "No more workpieces left to be processed.";
+    message1.style.color = "#333";
+    message1.style.fontSize = "24px";
+    message1.style.fontWeight = "600";
+    message1.style.marginBottom = "15px";
+    messageContainer.appendChild(message1);
 
+    // Second message
+    const message2 = document.createElement("p");
+    message2.textContent = "Reload the page to review analysis.";
+    message2.style.color = "#555";
+    message2.style.fontSize = "18px";
+    message2.style.fontWeight = "500";
+    message2.style.marginBottom = "15px";
+    messageContainer.appendChild(message2);
+
+    // Third message with highlighted tardiness
+    const message3 = document.createElement("p");
+    message3.innerHTML =
+      "You have a latency of <span style='color: #e74c3c; font-weight: bold;'>" +
+      tardiness +
+      " seconds</span>.";
+    message3.style.color = "#333";
+    message3.style.fontSize = "20px";
+    message3.style.fontWeight = "500";
+    message3.style.marginBottom = "20px";
+    messageContainer.appendChild(message3);
+
+    if (timeremaining >=0 && tardiness >= 0) {
+      const message4 = document.createElement("p");
+      message4.textContent =
+        "You will be able to finish the manufacturing on time. " ;
+      message4.style.color = "#50C878"; // Green for success
+      message4.style.fontSize = "18px";
+      message4.style.fontWeight = "500";
+      message4.style.marginBottom = "15px";
+      messageContainer.appendChild(message4);
+    } else {
+      const message4 = document.createElement("p");
+      message4.textContent =
+        "You will NOT be able to finish the manufacturing on time due to several delays." ;
+      message4.style.color = "#e74c3c"; // Red for failure
+      message4.style.fontSize = "18px";
+      message4.style.fontWeight = "500";
+      message4.style.marginBottom = "15px";
+      messageContainer.appendChild(message4);
+    }
+    
+
+    
+
+    // Append the message container to the body
     document.body.appendChild(messageContainer);
-  }, 500);
+  }, 500); // Increased delay for better transition
 
+  // Add spinner animation styles
   const style = document.createElement("style");
   style.textContent = `
         @keyframes spin {
             0% { transform: rotate(0deg); }
             100% { transform: rotate(360deg); }
         }
+        body {
+            margin: 0;
+            padding: 0;
+            background: linear-gradient(120deg, #f6f9fc, #eef2f3);
+        }
     `;
   document.head.appendChild(style);
 }
+
 
 let nor = 10;
 async function fetchExcelData() {
@@ -410,7 +518,7 @@ async function fetchExcelData() {
       // }
 
       // Check if video time has reached a certain point and add/update rows in the table
-      if (rowi + 1 === 20) {
+      if (noofjobsrem === 0) {
         handleNoMoreWorkpieces(); // Clear page and display message
         return;
       }
@@ -424,7 +532,7 @@ async function fetchExcelData() {
     });
 }
 
-const at = [1.96, 12.86, 26.84, 33.31, 44.90, 54];
+const at = [1.96, 12.86, 26.84, 33.31, 44.9, 54];
 let atp = 0;
 
 // Function to create a new row in the table
@@ -510,7 +618,7 @@ loadExcelAndGenerateCharts();
 
 populateRandomDetails();
 
-document.getElementById("workpiece-column").textContent = rowi + 1;
+document.getElementById("workpiece-column").textContent = rowi + 2;
 function onButtonClick() {
   if (barLineChart) {
     barLineChart.destroy();
@@ -553,9 +661,31 @@ function onButtonClick() {
   video.currentTime = 0;
   video.play();
 
+
+
+  
   rowi++;
-  document.getElementById("workpiece-column").textContent = rowi + 1;
+  
+  noofjobsrem--;
+// Update timeremaining and ensure it is an integer
+timeremaining = Math.round(timeremaining - designcycletime) > 0  ? Math.round(timeremaining - designcycletime) : 0;
+
+
+// Update tardiness and ensure it is an integer
+tardiness = tardiness + Math.round(
+  (54 - designcycletime > 0 || isNaN(designcycletime)) 
+  ? 0 
+  : designcycletime - 54
+);
+
+
+
+  
+  document.getElementById("jobs").textContent = noofjobsrem-1;
+  document.getElementById("remaining-shift-time-column").textContent = timeremaining + " seconds";
+  document.getElementById("tardiness").textContent = tardiness + " seconds";
+
+  document.getElementById("workpiece-column").textContent = rowi + 2;
   fetchExcelData();
   loadExcelAndGenerateCharts();
 }
-
